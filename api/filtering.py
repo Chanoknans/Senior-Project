7071136301e851a0edc70d9282176cd1716b3ce5
3lVv0w <krseisenh@gmail.com>
2021-11-21 16:00:08 +0700

    update python api

--- api/filtering.py
+++ api/filtering.py
@@ -9,38 +9,100 @@ Original file is located at
 
 import numpy as np
 import math
+import logging
+import os
+import json
 import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import var
 import pandas as pd
 import scipy.optimize
 from scipy import signal
 from scipy import interpolate
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
+from urllib.parse import urlparse
 
 #google cloud session
-from google.cloud import firestore
-from google.cloud.firestore import ArrayUnion
 from datetime import datetime, timezone
 import time
 import pytz
+import paho.mqtt.client as mqtt
 import firebase_admin
-from firebase_admin import credentials,firestore
+from dotenv import load_dotenv
+from firebase_admin import credentials, firestore
+from pydash import get
 
-cred = credentials.Certificate("C:/Users/chanoknan/Senior Project/senior_project.json")
+# recived incoming data ??
+cred = credentials.Certificate("./senior_project.json")
 firebase_admin.initialize_app(cred)
-
-#retrieve value from firestoregit remote add origin https://github.com/Chanoknans/Hearing-Aid-Server.git
 db = firestore.client()
-users_ref = db.collection(u'users').document(u'Audiogram')
-doc = users_ref.get()
-if doc.exists:
-    print(f"Document data: {doc.to_dict()}")
-else:
-    print("No such document!")
-result = doc.to_dict();
-newval = list(result.values())
-newval2 = newval[0]
-#print(newval2)
+
+host = os.getenv('CLOUDMQTT_URL')
+url = urlparse(host)
+topic = url.path[1:]
+
+print("hi")
+
+def on_connect(self, client, userdata, rc):
+    logging.info('connected')
+    self.subscribe(topic, 0)
+
+
+def on_message(client, userdata, msg):
+    logging.debug(msg.payload.decode("utf-8", "strict"))
+    m_in = json.loads(msg.payload.decode("utf-8", "strict"))
+    user_id = get(m_in, 'u_id')
+    a_id = get(m_in, 'a_id')
+    # extract
+    # TODO: => extract data
+    result = m_in.to_dict();
+    newval = list(result.values())
+    newval2 = newval[0]
+
+    # processing data
+    #fminserch
+    banana = lambda x: ErrorFunction(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8], newval2)
+    opt = scipy.optimize.fmin(func=banana, x0=[1599,0.299,9.5,4000,0.899,10,8000,0.5,10],xtol=1e-4,ftol=1e-4,full_output=True) #,full_output=True to see all outputs [1500,0.3,10,4000,0.9,10,8000,0.5,10]
+    print('Current function value: ',opt[1])
+    print('Iterations: ',opt[2])
+    print('Function evaluations: ',opt[3])
+
+    xopt = opt[0]
+    print('Coefficients: ',xopt); #https://stackoverflow.com/questions/28167648/seeking-convergence-with-optimize-fmin-on-scipy
+    flpf = abs(xopt[0])
+    Qlpf = abs(xopt[1])
+    Glpf = abs(xopt[2])
+    fbpf = abs(xopt[3])
+    Qbpf = abs(xopt[4])
+    Gbpf = abs(xopt[5])
+    fhpf = abs(xopt[6])
+    Qhpf = abs(xopt[7])
+    Ghpf = abs(xopt[8])
+    q,h_lpf = BiquadLPF(flpf,Qlpf)
+    H_lpf = h_lpf*Glpf
+    q,h_bpf = BiquadBPF(fbpf,Qbpf)
+    H_bpf = h_bpf*Gbpf
+    q,h_hpf = BiquadHPF(fhpf,Qhpf)
+    H_hpf = h_hpf*Ghpf
+
+    desired_flt = abs(H_lpf+H_bpf+H_hpf)
+    h_desired = 20*np.log10(desired_flt)
+    octave_freq = [250,500,1000,2000,4000,8000]
+    threshold_dB = newval2 #[20,20,10,10,10,10] #[0,0,10,20,20,30]
+    spc = np.linspace(250,8000,1000)
+    h_ideal = np.interp(spc,octave_freq,threshold_dB)
+
+    #sending patient's data back to firebase
+    UTC = pytz.utc
+    coeff = xopt.tolist()
+
+    # update back to DB
+    doc_result = db.collection(u'users').document(user_id).collection(u'audiogram_history').document(a_id)
+    doc_result.update({
+        u'coeff': coeff, # array
+        u'updated_at': datetime.now(UTC),
+    })
+
 
 def BiquadLPF(flpf,Qlpf):
     f_samp = 20000
@@ -55,9 +117,8 @@ def BiquadLPF(flpf,Qlpf):
     num1= a1/b1[0]
     den1 = b1/b1[0]
     q1,h1 = signal.freqz(num1,den1,worN=1000,fs=20000)#fs=16000
-    #plt.plot(q1,abs(h1));plt.grid()
     return q1,h1
-#BiquadLPF(4000,0.707)
+
 def BiquadBPF(fbpf,Qbpf):
     f_samp = 20000
     ohm0 = np.tan(np.pi*fbpf/f_samp)
@@ -71,9 +132,8 @@ def BiquadBPF(fbpf,Qbpf):
     num1= a1/b1[0]
     den1 = b1/b1[0]
     q1,h1 = signal.freqz(num1,den1,worN=1000,fs=20000)
-    #plt.plot(q1,abs(h1));plt.grid()
     return q1,h1
-#BiquadBPF(4000,0.707)
+
 def BiquadHPF(fhpf,Qhpf):
     f_samp = 20000
     ohm0 = np.tan(np.pi*fhpf/f_samp)
@@ -87,74 +147,33 @@ def BiquadHPF(fhpf,Qhpf):
     num1= a1/b1[0]
     den1 = b1/b1[0]
     q1,h1 = signal.freqz(num1,den1,worN=1000,fs=20000)
-    #plt.plot(q1,abs(h1));plt.grid()
     return q1,h1
-#BiquadHPF(4000,0.707)
-def ErrorFunction(fL,QL,GL,fB,QB,GB,fH,QH,GH):
+
+def ErrorFunction(fL,QL,GL,fB,QB,GB,fH,QH,GH, newval2):
     q,h_lpf = BiquadLPF(fL,QL)
     HL = h_lpf*GL
     q,h_bpf = BiquadBPF(fB,QB)
-    HB = h_bpf*GB 
+    HB = h_bpf*GB
     q,h_hpf = BiquadHPF(fH,QH)
     HH = h_hpf*GH
     desired_flt = abs(HL+HB+HH)
-    #print(desired_flt);print(np.shape(desired_flt))
     h_desired = 20*np.log10(desired_flt)
     octave_freq = [250,500,1000,2000,4000,8000]
-    threshold_dB = newval2 #[20,20,10,10,10,10]my#[0,0,10,20,20,30]
+    threshold_dB = newval2
     spc = np.linspace(250,8000,1000)
     h_ideal = np.interp(spc,octave_freq,threshold_dB)
     err = mean_absolute_error(h_ideal,h_desired)
-
-    #print(err)
     return err
 
-#fminserch
-banana = lambda x: ErrorFunction(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8])
-opt = scipy.optimize.fmin(func=banana, x0=[1599,0.299,9.5,4000,0.899,10,8000,0.5,10],xtol=1e-4,ftol=1e-4,full_output=True) #,full_output=True to see all outputs [1500,0.3,10,4000,0.9,10,8000,0.5,10]
-print('Current function value: ',opt[1])
-print('Iterations: ',opt[2])
-print('Function evaluations: ',opt[3])
 
-xopt = opt[0]
-print('Coefficients: ',xopt); #https://stackoverflow.com/questions/28167648/seeking-convergence-with-optimize-fmin-on-scipy
-flpf = abs(xopt[0])
-Qlpf = abs(xopt[1])
-Glpf = abs(xopt[2])
-fbpf = abs(xopt[3])
-Qbpf = abs(xopt[4])
-Gbpf = abs(xopt[5])
-fhpf = abs(xopt[6])
-Qhpf = abs(xopt[7])
-Ghpf = abs(xopt[8])
-q,h_lpf = BiquadLPF(flpf,Qlpf)
-H_lpf = h_lpf*Glpf
-q,h_bpf = BiquadBPF(fbpf,Qbpf)
-H_bpf = h_bpf*Gbpf
-q,h_hpf = BiquadHPF(fhpf,Qhpf)
-H_hpf = h_hpf*Ghpf
+def on_subscribe(client, obj, mid, granted_qos):
+    logging.debug(f'Subscribed: {str(mid)} {str(granted_qos)}')
 
-desired_flt = abs(H_lpf+H_bpf+H_hpf)
-h_desired = 20*np.log10(desired_flt)
-octave_freq = [250,500,1000,2000,4000,8000]
-threshold_dB = newval2 #[20,20,10,10,10,10] #[0,0,10,20,20,30]
-spc = np.linspace(250,8000,1000)
-h_ideal = np.interp(spc,octave_freq,threshold_dB)
 
-#sending patient's data back to firebase
-UTC = pytz.utc
-coeff = xopt.tolist()
-#print(coeff)
-doc_result = db.collection(u'users').document(u'coefficient')
-doc_result.set({
-    u'coeff': coeff,
-    u'timestamp': datetime.now(UTC),
-    u'username': u'Chanoknan'
-})
+def on_disconnect(client, userdata, rc):
+    logging.debug(f'disconnecting reason {str(rc)}')
+    client.connected_flag = False
+    client.disconnect_flag = True
 
-plt.plot(q+250,h_desired,'r')
-plt.plot(q+250,h_ideal,'g')
-plt.grid();plt.xlabel('Frequency (Hz)');plt.ylabel('Hearing Level (dB HL)')
-plt.xlim([100, 8000])
-plt.legend(['desired','actual'])
-plt.show()
+
+
